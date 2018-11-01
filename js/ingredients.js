1b8c1059634c0a51a32524f303433bbed3445614
Peter Phan <p2phan@ucsd.edu>
2018-10-31 22:38:57 -0700

    finished list page

--- js/ingredients.js
+++ js/ingredients.js
@@ -1,1 +1,29 @@
-alert("wha")
+
+
+(function(window, document, undefined){
+
+    // code that should be taken care of right away
+    
+    window.onload = init;
+    
+      function init(){
+        // the code to be called when the dom has loaded
+        // #document has its nodes
+        let ingredients = ["bacon", "eggs", "milk"];
+
+        var list = document.getElementById("list");
+        for(var i = 0; i<ingredients.length; i++){
+            var addToList = '<div>'
+            + '<button type="button" class="btn btn-light" disabled>'
+            + ingredients[i]
+            + '</button>'
+            + ' <button type="button" class="btn btn-danger">'
+            + '<i class="fas fa-minus"></i>'
+            + '</button>'
+            + '</div>';
+            list.innerHTML += addToList;
+        }
+
+      }
+    
+    })(window, document, undefined);
