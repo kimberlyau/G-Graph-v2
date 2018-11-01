3c0e2a43f477158697f07a8bae83246f141501cb
kimberlyau <kimberlyau@users.noreply.github.com>
2018-10-31 22:38:47 -0700

    Intermediate dev for location page

--- js/location.js
+++ js/location.js
@@ -1,15 +1,27 @@
 function myFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
+  var input, filter, ul, li, a, i;
+
+  // Text input
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+
+  // Clear if empty
+  if (filter.length == 0) {
+    for (i = 0; i < li.length; i++) {
+          li[i].style.display = "none";
+        }
+    } else {
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];
-        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
+        // Have to remove <br>, bold tags
+        if (a.innerHTML.replace('<br>', '').replace('<b>', '').replace('</b>', '').toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
         } else {
             li[i].style.display = "none";
         }
     }
+  }
 }
