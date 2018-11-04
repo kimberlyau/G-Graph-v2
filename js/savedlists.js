e06cdfef53972d16bd8dfcf972276c3741258ed7
Emily Le <emilycharlottele@yahoo.com>
2018-11-03 22:42:51 -0700

    changes to saved list

--- js/savedlists.js
+++ js/savedlists.js
@@ -2,74 +2,54 @@ var listFood = JSON.parse(localStorage.getItem("myList"));
 
 function showIngredientsList() {
     var showIngred = document.getElementById("testing");
-    showIngred.innerHTML="";
-    //var ul = document.getElementById("ingredLists");
+    showIngred.innerHTML = "";
     showIngred.innerHTML += "<ul>";
-    for(var i = 0; i<listFood.length; i++){
-        var foodBullets = document.createElement("LI");
+    for (var i = 0; i < listFood.length; i++) {
+        var foodBullets = document.createElement("li");
         foodBullets.innerHTML = listFood[i];
         showIngred.appendChild(foodBullets);
-        console.log(showIngred.innerHTML);
-
-        //var t = document.createTextNode(listFood[i]);
-        //foodBullets.appendChild(t);
-        //document.body.appendChild(foodBullets);
     }
     showIngred.innerHTML += "</ul>";
-
 }
-//console.log(showIngred);
-(function(window, document, undefined){
+
+
+
+function accordionIngredients() {
+    var showIngred = document.getElementById("dynamicAcc");
+    var $template = $(".template");
+
+    var hash = 2;
+    $("#dynamicAcc").on("click", function () {
+        var $newPanel = $template.clone();
+        $newPanel.find(".collapse").removeClass("in");
+        $newPanel.find(".accordion-toggle").attr("href", "#" + (++hash))
+            .text("Dynamic panel #" + hash);
+        $newPanel.find(".panel-collapse").attr("id", hash);
+        $("#accordion").append($newPanel.fadeIn());
+    });
+}
+
+var saveAccordion;
+function saveAcc() {
+    localStorage.setItem('saveAccordion', saveAccordion);
+}
+
+
+
+
+
+
+(function (window, document, undefined) {
 
     // code that should be taken care of right away
-    
+
     window.onload = init;
-    
-      function init(){
+
+    function init() {
         // the code to be called when the dom has loaded
         // #document has its nodes
         showIngredientsList();
 
-      }
-    
-    })(window, document, undefined);
+    }
 
-function deleteList() {
-    var ul = document.getElementById("ingredLists");
-    for(var i = 0; i<listFood.length; i++){
-        var addToList = '<div>'
-        + '<button type="button" class="btn btn-light" disabled>'
-        + listFood[i]
-        + '</button>'
-        + ' <button id="'
-        + i
-        + '" type="button" class="btn btn-danger right" onclick="deleteItem(this)">'
-        + '<i class="fas fa-minus"></i>'
-        + '</button>'
-        + '</div>';
-        ul.innerHTML += addToList;
-      }
-}
-
-function deleteItem(elem){
-    var getItem = $(elem).attr('id');
-    var index = myList.indexOf(getItem);
-    myList.splice(index, 1);
-    show[index]=false;
-    displayIngredients();
-  }
-
-  (function(window, document, undefined){
-
-    // code that should be taken care of right away
-    
-    window.onload = init;
-    
-      function init(){
-        // the code to be called when the dom has loaded
-        // #document has its nodes
-        showIngredientsList();
-
-      }
-    
-    })(window, document, undefined);
+})(window, document, undefined);
