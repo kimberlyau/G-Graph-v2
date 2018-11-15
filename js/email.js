6605f116c9d3ab40693db4aa562e31ab04f7e095
kimberlyau <kimberlyau@users.noreply.github.com>
2018-11-15 01:28:32 -0800

    recipe.html ingredients finished, need modal. comforta added

--- js/email.js
+++ js/email.js
@@ -32,9 +32,9 @@ function initModal(){    //Taken from w3schools
     var modal2 = document.getElementsByClassName("modal-body")[1];
 
 
-    
 
-    // When the user clicks on the button, open the modal 
+
+    // When the user clicks on the button, open the modal
     saveAs.onclick = function() {
         modal.style.display = "block";
 
@@ -73,9 +73,9 @@ function initModal(){    //Taken from w3schools
     // Get the input in modal
     var currListName = localStorage.getItem("listName");
     var listName = document.getElementById('listName');
-    if(currListName === ""){    
+    if(currListName === ""){
         var lengthL = JSON.parse(localStorage.getItem("savedLists")).length
-        listName.value = "List " + (lengthL+1); 
+        listName.value = "List " + (lengthL+1);
     }
     else{
         listName.value = currListName;
@@ -91,11 +91,11 @@ function saveList(){
     var myList = JSON.parse(localStorage.getItem("myList"));
     var listName = document.getElementById('listName');
 
-    if(myList === []){ 
-        alert("There is nothing on your list please add something to it"); 
+    if(myList === []){
+        alert("There is nothing on your list please add something to it");
         return;
     }
-    
+
     var selectedStore = localStorage.getItem("selectedStore");
     var newList = [];
     newList.push(listName.value);
@@ -105,13 +105,13 @@ function saveList(){
     console.log(overwriteTo)
     if(overwriteTo >= 0){
         savedLists[overwriteTo] = newList;
-        
+
     }
     else{
         savedLists.push(newList);
     }
     console.log(savedLists);
-    
+
     localStorage.setItem("savedLists", JSON.stringify(savedLists));
 }
 
@@ -121,7 +121,7 @@ function displaySummary(){
     var myList = JSON.parse(localStorage.getItem("myList"));
     var add ='<div>'
         +  '<div class="panel-body" id="testing">'
-        +   '<b>Store:</b> ' + selectedStore 
+        +   '<b>Store:</b> ' + selectedStore
         +    '<br><div><b>List:</b><ul>';
         for(var j =0; j<myList.length; j++){
             add += '<li>' + myList[j] + '</li>';
@@ -134,23 +134,23 @@ function displaySummary(){
     summary.innerHTML = add;
 
 }
-  
+
 (function(window, document, undefined){
-  
+
       // code that should be taken care of right away
-      
+
       window.onload = init;
-  
-      
+
+
         function init(){
           // the code to be called when the dom has loaded
           // #document has its nodes
           displaySummary();
           initModal();
 
-  
+
         }
-  
-        
-      
+
+
+
   })(window, document, undefined);
