1c4e05302b65a35fa879f56ee3de9594d811ea0c
kimberlyau <kimberlyau@users.noreply.github.com>
2018-12-01 23:03:50 -0800

    storemap re-redesign

--- js/storemap.js
+++ js/storemap.js
@@ -29,11 +29,29 @@ function togglePin (elmnt, index) {
 
         var list = document.getElementById("list");
         for(var i = 0; i<ingredients.length; i++){
-            var addToList = '<div class="form-check">'
-            + `<input type="checkbox" class="form-check-input" id="materialUnchecked" onchange="togglePin(this, ${i + 1})">`
-            + ingredients[i]
-            + '</div>'
-            list.innerHTML += addToList;
+          var rnum = Math.floor(Math.random() * 20) + 1;
+          var multDash = 25;
+          var spacer = '-'.repeat(multDash);
+          var quant = 'Pound';
+
+          if (ingredients[i] == 'milk' || ingredients[i] == 'bacon') {
+            quant = 'Quantity';
+          }
+
+          var addToList = '<div class="form-check">'
+          + `<input type="checkbox" class="form-check-input" id="materialUnchecked" onchange="togglePin(this, ${i + 1})">`
+          + ingredients[i]
+          + '   |   $'
+          + Math.floor(Math.random() * 5)
+          + '.'
+          + (Math.floor(Math.random() * 90) + 10)
+          + ' Per ' + quant + '<br>'
+          + spacer
+          + ' <i>Aisle '
+          + rnum
+          + '</i>'
+          + '</div>'
+          list.innerHTML += addToList;
         }
 
       }
